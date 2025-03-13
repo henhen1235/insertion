b70db88a3341fdb0c0e7a4e43fcf839d568b0295
U-BSD\514039 <514039@SHS-95W2NW3.beaverton.k12.or.us>
2025-03-13 15:29:42 -0700

    more work at school

--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
+#include <fstream>
 #include "dNode.h"
+#include <cstring>
+#include <cstdlib>
 
 using namespace std;
 
 void addfunc(dNode *& treehead);
+void placenode(int newint, dNode *& headnode);
+void display(dNode* top);
+void displayrunner(dNode* tempposition, int depth);
 
 int main(){
     dNode* treehead = NULL;
     while(true){
-         char i2n[50];// ask user for what they want to do
+      char i2n[50]; // ask user for what they want to do
         char add[50] = "add";
         char remove[50] = "remove";
         char display[50] = "display";
         cout << endl << "What would you like to do?: ";
-        cin >> i2n;
+        cin.getline(i2n, 50);
         if(strcmp(i2n, add) == 0){
             addfunc(treehead);
         }
         else if(strcmp(i2n, remove) == 0){
         }
         else if(strcmp(i2n, display) == 0){
-        }
+	  display(treehead);
+	}
         else{
             cout << "Invalid input options are: add, remove, and display" << endl;
         }
@@ -33,44 +40,112 @@ void addfunc(dNode *& treehead){
     char con[50] = "console";
     char fil[50] = "file";
     cout << endl << "Would you rather read in from console or file?: ";
-    cin >> in;
+    cin.getline(in, 50);
     if(strcmp(in, con) == 0){
         char num[50];
         cout << "Enter the series of numbers you would like to add: ";
-        cin >> num;
+        cin.getline(num, 50);
+	int spot = strlen(num);
+	num[spot] = ' ';
+	num[spot + 1] = '\0';
         int pos = 0;
         int tempnumpos = 0;
         int numarr[50];
         int numarrpos = 0;
-        while(true){
-            char tempnum[4];
-            if(num[pos] != ' '){
+
+        while(num[pos] != '\0'){
+            char tempnum[10];
+            if(num[pos] != ' ' && num[pos] != '\0'){
                 tempnum[tempnumpos] = num[pos];
                 tempnumpos++;
                 pos++;
             }
-            else if(num[pos] == ' '){
+            else if(num[pos] == ' ' || num[pos] == '\0'){
                 tempnum[tempnumpos] = '\0';
                 tempnumpos = 0;
-                int finnum = (int)tempnum - 48;
+                int finnum = atoi(tempnum);
                 numarr[numarrpos] = finnum;
                 numarrpos++;
                 pos++;
-                cout << tempnum << endl;
             }
             else{
                 break;
             }
         }
+	
         for(int i = 0; i < numarrpos; i++){
-            cout << numarr[i] << endl;
+	  placenode(numarr[i]);
         }
     }
     else if(strcmp(in, fil) == 0){
-
+      cout << "Please type file name" << endl;
+      string name;
+      getline(cin, name);
+      string txt = ".txt";
+      ifstream inputFile((name + txt));
+      int num;
+      while (inputFile >> num) {
+        placenode(num);
+      }
+      inputFile.close();
     }
     else{
         cout << "Invalid input options are: console, and file" << endl;
     }
 
 }
+
+void placenode(int newint, dNode *& headnode){
+  if(headnode == NULL){
+    dNode* newnode= new dNode(headnode);
+    headnode = newnode;
+    break;
+  }
+
+  dNode* tempnode = headnode;
+  while(true){
+  if(tempnode->getint() > newint){
+    if(tempnode->getLeft() == NULL){
+      dNode* newnode = new dNode(headnode);
+      tempnode->setLeft(newnode);
+      break;
+    }
+    tempnode = tempnode->getLeft();
+  }
+  else(tempnode->getint() <= newint){
+    if(tempnode->getRight() == NULL){
+      dNode* newnode = new dNode(headnode);
+      tempnode->setRight(newnode);
+      break;
+    }
+    tempnode = tempnode->getRight();
+  }
+  }
+}
+
+void display(dNode* top){//diplay everything
+  if (top == NULL) {
+    return;
+  }
+  int depth = 0;
+  int start = 1;
+  displayrunner(top, depth);
+}
+
+void displayrunner(dNode* tempposition, int depth){// display runner function
+  if(tempposition->getLeft() == NULL && tempposition->getRight() == NULL){
+    return;
+  }
+
+  if(tempposition->getLeft() != NULL){//go down the right nodes
+    displayrunner(tempposition->getLeft(), depth + 1);
+  }
+  for(int x = 0; x < depth; x++){//orint
+    cout << '\t';
+  }
+  cout << tempposition->getint() << endl;
+  if(tempposition->getRight() != NULL){//go down the left nodes
+    displayrunner(tempposition->getRight(), depth + 1);
+  }
+}
+ 
